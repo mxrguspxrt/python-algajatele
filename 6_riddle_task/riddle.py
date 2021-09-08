c3d4cd730ef1e1762f734216a721cb19ee038194
Margus Pärt <margus@wave.ee>
2021-09-08 21:53:11 +0300

    save

---
+++ 6_riddle_task/riddle.py
@@ -0,0 +1,22 @@
+# This task is to introduce arrays/strings/conditions in Python. Also some logical thinking.
+
+# Each of the ? must be replaced with a char, that is unique (so can't be same as on left or right).
+
+def solve_riddle(str):
+  return "tereexff" 
+
+
+def run_tests():
+  is_correct_solution(solve_riddle("abc?def"))
+  is_correct_solution(solve_riddle("abc?de?????"))
+  is_correct_solution(solve_riddle("??????"))
+
+def is_correct_solution(solved_str):
+  if "?" in solved_str:
+    raise RuntimeError("Solution can't contain ?, but was '" + solved_str + "'")
+  
+  for i in range(len(solved_str)-1):
+    if solved_str[i] == solved_str[i+1]:
+      raise RuntimeError("Solution can't contain 2 same chars, but was '" + solved_str + "'")
+
+run_tests()      
---
+++ README.md
@@ -0,0 +1,6 @@
+# 02.09.2021
+1-4 
+5 - put the red dot to work on x and y axis
+
+# 09.09.2021
+6 - test to measure knowledge of logic constructs (see 2)
