0e02d282bde6666d1ec056c2c08a5a9e5c7747dc
Margus Pärt <margus@wave.ee>
2021-09-29 23:39:25 +0300

    Create hanoi_towers.py

---
+++ 9_hanoi_towers/hanoi_towers.py
@@ -0,0 +1,24 @@
+# TASK:
+#
+# Please read: https://en.wikipedia.org/wiki/Tower_of_Hanoi
+
+# disks stacked on one rod in order of decreasing size, the smallest at the top
+towers = [ 
+    [5, 4, 3, 2, 1],
+    [],
+    []
+]
+
+# The objective of the puzzle is to move the entire stack to the last rod, obeying the following rules:
+#
+# 1. Only one disk may be moved at a time.
+# 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
+# 3. No disk may be placed on top of a disk that is smaller than it.
+
+# please implement function to solve to puzzle
+
+def solve_puzzle():
+    ... 
+
+
+solve_puzzle()
