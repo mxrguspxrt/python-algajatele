a9f77b17303c4ddd5dcc5aa8ce1237464476e8a1
Margus Pärt <margus@wave.ee>
2021-10-21 10:28:01 +0300

    saved 10 tictactoe

--- 10_summary_of_learned/summary.py
+++ 10_summary_of_learned/summary.py
@@ -1,15 +1,36 @@
-ruut1 = "X"
-ruut2 = " "
-ruut3 = " "
-ruut4 = " "
-ruut5 = "X"
-ruut6 = " "
-ruut7 = " "
-ruut8 = " "
-ruut9 = "X"
+empty_square = " "
 
-print(ruut1 + " | " + ruut2 + " | " + ruut3)
-print("----------")
-print(ruut4 + " | " + ruut5 + " | " + ruut6)
-print("----------")
-print(ruut7 + " | " + ruut8 + " | " + ruut9)
+squares = [
+  empty_square, empty_square, empty_square,
+  empty_square, empty_square, empty_square,
+  empty_square, empty_square, empty_square
+]
+
+def print_tic_tac_toe_table():
+  print(squares[0] + " | " + squares[1] + " | " + squares[2])
+  print("----------")
+  print(squares[3] + " | " + squares[4] + " | " + squares[5])
+  print("----------")
+  print(squares[6] + " | " + squares[7] + " | " + squares[8])
+
+def ask_move_to_make(x_or_y):
+  move_to_make = input("Kuhu soovid lisada " + x_or_y + "?: ")
+
+  move_to_make_int = int(move_to_make) - 1
+
+  if squares[move_to_make_int] != empty_square:
+    print("Cant make move, because already contains: " + squares[0])
+    return False
+  else:
+    squares[move_to_make_int] = x_or_y
+    return True
+
+
+while True:
+  while not ask_move_to_make("X"):
+    print_tic_tac_toe_table()
+  print_tic_tac_toe_table()
+  while not ask_move_to_make("Y"):
+    print_tic_tac_toe_table()
+  print_tic_tac_toe_table()
+
--- 10_summary_of_learned/summary_cheats.py
+++ 10_summary_of_learned/summary_cheats.py
@@ -165,3 +165,40 @@ for i in range(0, len(animals)):
   print("i = ", i)
   print(animals[i]["name"], animals[i]["age"], " ja ta on ", animals[i]["color"]," värvi")
 
+
+
+
+
+
+
+empty_square = " "
+
+squares = [
+  empty_square, empty_square, empty_square,
+  empty_square, empty_square, empty_square,
+  empty_square, empty_square, empty_square
+]
+
+def print_tic_tac_toe_table():
+  print(squares[0] + " | " + squares[1] + " | " + squares[2])
+  print("----------")
+  print(squares[3] + " | " + squares[4] + " | " + squares[5])
+  print("----------")
+  print(squares[6] + " | " + squares[7] + " | " + squares[8])
+
+def ask_move_to_make(x_or_y):
+  move_to_make = input("Kuhu soovid lisada " + x_or_y + "?: ")
+
+  move_to_make_int = int(move_to_make) - 1
+
+  if squares[move_to_make_int] != empty_square:
+    print("Cant make move, because already contains: " + squares[0])
+  else:
+    squares[move_to_make_int] = x_or_y
+
+
+while True:
+  ask_move_to_make("X")
+  print_tic_tac_toe_table()
+  ask_move_to_make("Y")
+  print_tic_tac_toe_table()
