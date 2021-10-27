750b2b902947db3dab9746225f31d6885431c6e7
Margus Pärt <margus@wave.ee>
2021-10-27 22:31:38 +0300

    added test

---
+++ 11_test_of_input_output_arrays_conditionals/README.md
@@ -0,0 +1,19 @@
+# Test #1
+
+## Sissejuhatus / Introduction
+
+ET:
+Test mõõdab oskust kasutada muutujaid, tingimuslauseid, lugeda ja muuta massiivi sisu.
+
+EN:
+This test measures skill to use variables, conditionals and read and change contents of an array. 
+
+
+# Seotud lingid / Related links
+
+* https://www.w3schools.com/python/python_file_open.asp
+* https://www.w3schools.com/python/python_file_write.asp
+* https://www.w3schools.com/python/python_user_input.asp
+* https://www.w3schools.com/python/python_for_loops.asp
+* https://www.w3schools.com/python/python_lists.asp (alamlingid: access, change, add, remove, loop, join)
+* https://www.w3schools.com/python/python_strings.asp (alamlink: contact)
---
+++ 11_test_of_input_output_arrays_conditionals/test.py
@@ -0,0 +1,43 @@
+# this function removes spaces and new lines for column from right and left
+def strip(string):
+    return string.strip()
+
+# this function reads database from contacts.txt file
+def read_database():
+    file = open("C:/Users/margu/Documents/GitHub/python-algajatele/11_test_of_input_output_arrays_conditionals/contacts.txt", encoding="utf-8")
+    rows = []
+    for row in file:
+        rows.append(list(map(strip, row.split(", "))))
+    return rows
+
+# this function writes contacts to file
+def write_database(db):
+    file = open("C:/Users/margu/Documents/GitHub/python-algajatele/11_test_of_input_output_arrays_conditionals/contacts.txt", encoding="utf-8")
+    rows = []
+    for row in db: 
+        rows.append(", ".join(row))
+    file.write("\n".join(rows))
+    file.close()
+
+# this function prints all contacts from db that is in memory
+def print_out_database(db):
+    print("Index \t Name \t\t Phone \t\t Age \t Email")
+    for i in range(0, len(db)):
+        row = db[i]
+        print(i, "\t", row[0], "\t", row[1], "\t", row[2], "\t", row[3], "\t")
+
+def print_out_commands():
+    while True:
+        print("Commands are:")
+        print("1. list users")
+        print("2. edit user")
+        print("3. add user")
+        input("What is your command?: ")
+
+def main():
+  db = read_database()
+  print_out_database(db)
+  print_out_commands()
+
+
+main()
